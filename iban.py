9bcb11dd8091dcab9652439d938155b4d99d6173
Celyne <dianakydd2@gmail.com>
2024-03-16 12:51:06 +0300

    Added comments

--- iban.py
+++ iban.py
@@ -22,4 +22,26 @@ else:
         print("IBAN entered is valid.")
     else:
         print("IBAN entered is invalid.")
+
+        '''line 03: ask the user to enter the IBAN (the number can contain spaces, as they significantly improve number readability...
+line 04: ...but remove them immediately)
+line 06: the entered IBAN must consist of digits and letters only – if it doesn't...
+line 07: ...output the message;
+line 08: the IBAN mustn't be shorter than 15 characters (this is the shortest variant, used in Norway)
+line 09: if it is shorter, the user is informed;
+line 10: moreover, the IBAN cannot be longer than 31 characters (this is the longest variant, used in Malta)
+line 11: if it is longer, make an announcement;
+line 12: start the actual processing;
+line 13: move the four initial characters to the number's end, and convert all letters to upper case (step 02 of the algorithm)
+line 14: this is the variable used to complete the number, created by replacing the letters with digits (according to the algorithm's step 03)
+line 15: iterate through the IBAN;
+line 16: if the character is a digit...
+line 17: just copy it;
+line 18: otherwise...
+line 19: ...convert it into two digits (note the way it's done here)
+line 20: the converted form of the IBAN is ready – make an integer out of it;
+line 21: is the remainder of the division of iban2 by 97 equal to 1?
+line 22: If yes, then success;
+line 23: Otherwise...
+line 24: ...the number is invalid.'''
     
